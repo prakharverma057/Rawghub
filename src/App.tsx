e5fabf0f4f578e6aa7626983a7e115e23037ac5b
Prakhar Verma <prakharverma.work@gmail.com>
2025-02-15 00:18:32 +0530

    nav bar initialisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Button } from "@chakra-ui/react";
-
+import { Grid, GridItem, Show, HStack, Text, Image } from "@chakra-ui/react";
+import logo from "./assets/logo.svg";
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid templateAreas={{ base: `"nav nav"`, lg: `"aside main"` }}>
+      <HStack>
+        <GridItem area={"nav"}>
+          <Image src={logo} boxSize="60px" />
+        </GridItem>
+        <Text>Header</Text>
+      </HStack>
+      <Show above="lg">
+        <GridItem area={"aside"} bgColor="lightblue">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bgColor="lightgreen">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
